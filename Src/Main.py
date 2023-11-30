02e74171fe824dfaf22638d7d55031f8bc72ab8a
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-11-30 14:39:28 -0500

    Create Main.py

---
+++ Src/Main.py
@@ -0,0 +1,14 @@
+from Evolve3D import Evolution
+from gui import EvolutionGUI
+from PyQt5.QtWidgets import QApplication
+import sys
+
+def main():
+    app = QApplication(sys.argv)
+    evolution = Evolution(population_size=100)
+    gui = EvolutionGUI(evolution)
+    gui.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
