9b5572a77313504da4b741ffdfd248e775503e43
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-12-03 17:26:24 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,22 +1,10 @@
-# main.py
 import threading
-import pygame
 from GUI import EvolutionGUI, Evolution
 
 def main():
-    pygame.init()  # Initialize pygame
-
     evolution = Evolution(population_size=100)
     gui = EvolutionGUI(evolution)
-
-    # Run the Tkinter main loop in a separate thread
-    tkinter_thread = threading.Thread(target=gui.run, daemon=True)
-    tkinter_thread.start()
-
-    # Wait for Tkinter thread to finish
-    tkinter_thread.join()
-
-    pygame.quit()  # Properly close pygame
+    gui.run()
 
 if __name__ == '__main__':
     main()
