d7a1fd849773af847f8a15cfed7db2dbdf14f09a
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-12-04 16:11:15 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 # main.py
 
+import os
+
+# Explicitly set the preferred toolkit before importing GUI modules
+os.environ['ETS_TOOLKIT'] = 'qt4'  # Use 'wx', 'qt4', or 'qt' as per your toolkit
+
 from evolution_gui import EvolutionGUI
 from evolution import Evolution
 
