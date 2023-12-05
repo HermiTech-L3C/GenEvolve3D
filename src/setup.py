24d9fe26fb3c3b6f9b3da7d89ff9f145cd8b1ecf
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-12-05 02:39:31 -0500

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,37 +1,40 @@
 from cx_Freeze import setup, Executable
 import sys
 
-# Define the base for the executable
-base = "Win32GUI" if sys.platform == "win32" else None
+def get_base():
+    return "Win32GUI" if sys.platform == "win32" else None
 
-# List of required packages
-required_packages = [
-    "numpy",
-    "PyQt5",  # Use "PyQt5" instead of "pyqt5"
-    "vispy"
-]
+def get_required_packages():
+    return [
+        "numpy",
+        "PyQt5",  # Use "PyQt5" instead of "pyqt5"
+        "vispy"
+    ]
 
-# Additional non-Python files that are needed
-additional_files = [
-    'evolution.py',
-    'evolution_gui.py'
-    # Add other necessary files here, like images, data files, etc.
-]
+def get_additional_files():
+    return [
+        'evolution.py',
+        'evolution_gui.py'
+        # Add other necessary files here, like images, data files, etc.
+    ]
 
-# Build options for cx_Freeze
-build_options = {
-    "packages": required_packages,
-    "include_files": additional_files
-}
+def build_options():
+    return {
+        "packages": get_required_packages(),
+        "include_files": get_additional_files()
+    }
 
-# Define the main script
-main_script = "main.py"
+def create_setup():
+    base = get_base()
+    main_script = "main.py"
 
-# Setup script for building the executable
-setup(
-    name="GenEvolve3D",
-    version="0.1",
-    description="A 3D Genetic Algorithm Visualization Tool",
-    options={"build_exe": build_options},
-    executables=[Executable(main_script, base=base)]
-)
+    return setup(
+        name="GenEvolve3D",
+        version="0.1",
+        description="A 3D Genetic Algorithm Visualization Tool",
+        options={"build_exe": build_options()},
+        executables=[Executable(main_script, base=base)]
+    )
+
+if __name__ == "__main__":
+    create_setup()
