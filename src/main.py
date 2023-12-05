a3f7eca9f59abb6edeebb543c2dd691c6f46badf
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-12-04 20:29:47 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,18 @@
 import os
-
-# Explicitly set the preferred toolkit before importing GUI modules
-# Change to 'qt' if using PyQt5
-os.environ['ETS_TOOLKIT'] = 'qt'
-
 from evolution_pythreejs import EvolutionPyThreejs
 from evolution import Evolution
 
+# Explicitly set the preferred toolkit before importing GUI modules
+os.environ['ETS_TOOLKIT'] = 'qt'
+
 def main():
-    evolution = Evolution(population_size=100)
+    evolution = Evolution(population_size=100, fitness_function=lambda genome: sum(gene.activity for gene in genome.genes))
     evolution_pythreejs = EvolutionPyThreejs(population_size=100)
 
-    while evolution_pythreejs.generation < 100:  # Adjust the number of generations to visualize
+    for _ in range(100):  # Adjust the number of generations to visualize
         evolution.run_generation()
-        evolution_pythreejs.update_status()
+        evolution_pythreejs.run_generation()
+        evolution_pythreejs.draw_gene_network()  # Visualize the best individual's gene network
 
     evolution_pythreejs.run()
 
