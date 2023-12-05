0471b2ee5f62303bae712d2f5b24ba54b60d1129
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-12-04 20:15:18 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
-# main.py
-
 import os
 
 # Explicitly set the preferred toolkit before importing GUI modules
 # Change to 'qt' if using PyQt5
 os.environ['ETS_TOOLKIT'] = 'qt'
 
-from evolution_gui import EvolutionGUI
+from evolution_pythreejs import EvolutionPyThreejs
 from evolution import Evolution
 
 def main():
     evolution = Evolution(population_size=100)
-    gui = EvolutionGUI(evolution)
-    gui.run()
+    evolution_pythreejs = EvolutionPyThreejs(population_size=100)
+
+    while evolution_pythreejs.generation < 100:  # Adjust the number of generations to visualize
+        evolution.run_generation()
+        evolution_pythreejs.update_status()
+
+    evolution_pythreejs.run()
 
 if __name__ == '__main__':
     main()
