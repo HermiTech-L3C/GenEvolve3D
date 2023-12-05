dafe1908f81d51758ffe90a08e6f147e08d39e9d
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-12-04 22:41:00 -0500

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -12,11 +12,15 @@ build_exe_options = {
     "include_files": include_files
 }
 
+# Specify the path to your main.py relative to the script location
+# In this case, it assumes main.py is in the same directory as setup.py
+main_script = "main.py"
+
 # Setup script for building the executable
 setup(
     name="GenEvolve3D",
     version="0.1",
     description="GenEvolve3D Application",
     options={"build_exe": build_exe_options},
-    executables=[Executable("main.py", base=None)]
+    executables=[Executable(main_script, base=None)]
 )
